842b48d655cc63c7af8210c42faa5d7468201aa7
Alessandro <alepaolinicaar@gmail.com>
2022-08-24 10:38:40 -0300

    Desafio 5

---
+++ Desafio-5/ejs/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = require('./routes/productos');
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`Servidor running on port ${PORT}`);
+});
